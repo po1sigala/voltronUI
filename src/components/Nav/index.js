4215846448ec3ecc88bdd4530d325b9c0556afc7
Your Name <alexsigala87@gmail.com>
2021-04-26 22:39:33 -0700

    put skeleton back after merging. added navbar, sample text, will start on first voltron page next and styling nav bar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -8,13 +8,13 @@ class NavBar extends Component {
             <div>
                 <Nav activeKey="/home">
                     <Nav.Item>
-                        <Nav.Link href="/home">Home</Nav.Link>
+                        <Nav.Link href="/home">Package Opening</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link href="/trending">Trending</Nav.Link>
+                        <Nav.Link href="/trending">Associating</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link href="/search">Search The Globe</Nav.Link>
+                        <Nav.Link href="/search">Accession Hold</Nav.Link>
                     </Nav.Item>
                 </Nav>
             </div>
