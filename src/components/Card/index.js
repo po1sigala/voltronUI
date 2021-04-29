44fc70bc30bb082f77f12cb45bc305fd677a05f9
Your Name <alexsigala87@gmail.com>
2021-04-28 20:31:07 -0700

    added a function to check clicks and a form to accept the rq number

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -8,14 +8,9 @@ class newCard extends Component {
         return (
             <div>
                 <Card style={{ width: "18rem" }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
                     <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title
-                            and make up the bulk of the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
+                        <Card.Title>{this.props.title}</Card.Title>
+                        <Card.Text></Card.Text>
                     </Card.Body>
                 </Card>
             </div>
--- src/pages/Accesion.js
+++ src/pages/Accesion.js
@@ -1,10 +1,35 @@
 import React, { Component } from "react";
-import Card from "../components/Card";
+import Card from "react-bootstrap/Card";
+import InputGroup from "react-bootstrap/InputGroup";
+import Button from "react-bootstrap/Button";
+import FormControl from "react-bootstrap/FormControl";
 class Accesion extends Component {
+    displayRQ = () => {
+        console.log("searching for theRQ");
+    };
     render() {
         return (
             <div>
-                <Card></Card>
+                <Card
+                    className="card"
+                    style={{ width: "18rem", height: "35rem", padding: "2%" }}
+                >
+                    <InputGroup className="mb-3">
+                        <InputGroup.Prepend>
+                            <InputGroup.Text>Rq</InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <FormControl />
+
+                        <InputGroup.Append>
+                            <Button
+                                variant="outline-secondary"
+                                onClick={this.displayRQ}
+                            >
+                                Enter
+                            </Button>
+                        </InputGroup.Append>
+                    </InputGroup>
+                </Card>
             </div>
         );
     }
